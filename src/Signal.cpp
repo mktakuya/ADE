7adebdc4c213e7c341c8adb859bc08d18aabd4f7
toshiemon18 <st12318@gmail.com>
2014-07-16 16:08:05 +0900

    いろいろ変更した
    定義してた定数を普通の変数して定義した。
    若干ノイズに誤差を入れて微妙なもにょもにょを実現した、超ハイテックなシーンデス。

---
+++ src/Signal.cpp
@@ -0,0 +1,47 @@
+#include "Signal.h"
+
+//--------------------------------------------------------------
+void Signal::setup(){
+
+	ofBackground(255);
+	ofEnableSmoothing();
+	ofEnableAlphaBlending();
+	ofSetCircleResolution(64);
+
+	ofScale(1, -1);
+
+	clocks = 300;
+	piece = 35;
+
+}
+
+//--------------------------------------------------------------
+void Signal::update(){
+
+	for(int i = 0; i < piece; i++){
+		randy[i] = ofRandom(0, 1);
+		randy[i] += 0.01;
+		randx[i] = ofRandom(0, 1);
+		randx[i] += 0.01;
+		randz[i] = ofRandom(0, 1);
+		randz[i] += 0.01;
+
+		pNoise[i] = ofNoise(randx[i], randy[i], randz[i]) * 15;
+	}
+
+}
+
+//--------------------------------------------------------------
+void Signal::draw(){
+
+	for(int i = 0; i < piece; i++){
+		for(int j = 1; j <= pNoise[i]; j++){
+			ofSetColor(0, 0, 0, 200);
+			ofFill();
+			ofRect(15 + 40 * i, ofGetHeight() - 40 * j, 20, 20);
+		}
+	}
+	ofSleepMillis(clocks);
+
+
+}
