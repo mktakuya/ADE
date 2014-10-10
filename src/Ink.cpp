f4c362b9827543e79b61d301962a1bd6fbf97f72
toshiemon18 <st12318@gmail.com>
2014-10-11 02:20:28 +0900

    Add Scene Ink
    インクぽたぽたな感じ。キーボード操作はなし。

---
+++ src/Ink.cpp
@@ -0,0 +1,34 @@
+#include "Ink.h"
+
+//--------------------------------------------------------------
+void Ink::setup(){
+
+	ofEnableSmoothing();
+	ofSetCircleResolution(64);
+	ofBackground(0);
+	c = circleDiam;
+	circles = 0;
+	d = 0.1;
+	for (int i = 0; i < 1000; i++) {
+		circleDiam[i] = ofRandom(20, 170);
+		circlePosition[i].set(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()));
+	}
+
+}
+
+//--------------------------------------------------------------
+void Ink::update(){
+
+	circles += d;
+	d += 0.01;
+
+}
+
+//--------------------------------------------------------------
+void Ink::draw(){
+
+	for (int i = 0; i < (int)circles; i++) {
+		ofCircle(circlePosition[i], circleDiam[i]);
+	}
+
+}
