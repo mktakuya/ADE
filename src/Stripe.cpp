0bb3c6d31a41284f39c50e683189d1d1f0eb3518
toshiemon18 <st12318@gmail.com>
2014-10-14 00:10:24 +0900

    3.times{ puts "Fixed" } Scene Stripe
    三度目の正直、mkの顔も三度までは宇宙の摂理。

--- src/Stripe.cpp
+++ src/Stripe.cpp
@@ -5,11 +5,7 @@ void Stripe::setup(){
 
 	ofEnableSmoothing();
 	ofBackground(255);
-	ofSetFrameRate(50);
 
-	rotateFlag = false;
-	even = true;
-	degree = 0;
 	colorType = 0;
 
 }
@@ -17,21 +13,11 @@ void Stripe::setup(){
 //--------------------------------------------------------------
 void Stripe::update(){
 
-	if(rotateFlag == false) {
-		if(colorType == 3) {
-			colorType = 0;
-		}
-		else
-			colorType += 1;
+	if(colorType == 3) {
+		colorType = 0;
 	}
-	else {
-		if (degree >= 360) {
-			rotateFlag = false;
-			colorType++;
-			degree = 0;
-		}
-		degree += 2;
-	}
+	else
+		colorType += 1;
 
 }
 
@@ -39,35 +25,12 @@ void Stripe::update(){
 void Stripe::draw(){
 	float color[4][3] = {{127, 177, 201}, {255, 210, 92}, {202, 228, 79}, {255, 195, 89}};
 
-	if (rotateFlag == false) {
-		for (int i = 0; i < ofGetWidth() / 60; i++){
-			if ((i + colorType)%2 != 0) 
-				ofSetColor(color[colorType][0], color[colorType][1], color[colorType][2]);
-			else 
-				ofSetColor(255);
-			ofRect(i * 60, 0, 60, ofGetHeight());
-		}
-		ofSleepMillis(500);
+	for (int i = 0; i < ofGetWidth() / 60; i++){
+		if ((i + colorType)%2 != 0) 
+			ofSetColor(color[colorType][0], color[colorType][1], color[colorType][2]);
+		else 
+			ofSetColor(255);
+		ofRect(i * 60, 0, 60, ofGetHeight());
 	}
-
-	else {
-		for (int i = 0; i < ofGetWidth() / 60; i++) {
-			if ((i + colorType)%2 != 0) 
-				ofSetColor(color[colorType][0], color[colorType][1], color[colorType][2]);
-			else 
-				ofSetColor(255);
-			ofPushMatrix();
-			ofTranslate(i * 60 - 30, 0);
-			ofRotateY(degree);
-			ofRect(-30, 0, 60, ofGetHeight());
-			ofPopMatrix();
-		}
-	}
-}
-
-//--------------------------------------------------------------
-void Stripe::keyPressed(int key){
-	if (key == OF_KEY_RETURN) {
-		rotateFlag = true;
-	}
+	ofSleepMillis(500);
 }
