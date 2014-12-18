4a3b3d92b02a5589f44064ab02abf7462770703c
toshiemon18 <st12318@gmail.com>
2014-12-18 16:50:50 +0900

    目視で恋に落ちた

--- src/Rectfall.cpp
+++ src/Rectfall.cpp
@@ -8,8 +8,8 @@ void Rectfall::setup(){
 	colors[0].r = 255; colors[0].g = 255; colors[0].b = 10;
 	colors[1].r = 20;	 colors[1].g = 246; colors[1].b = 255;
 	colors[2].r = 250; colors[2].g = 0; 	   colors[2].b = 191;
-	length = ofGetWidth() / 4.0;
-	height = ofGetHeight() * 0.15;
+	triangle_interval = ofGetWidth() / 4.0;
+	triangle_locate_height = ofGetHeight() * 0.15;
 	radius = 80;
 	for (int i = 0; i < 3; i++) {
 		triangleDegree[i] = 90;
@@ -43,15 +43,15 @@ void Rectfall::update(){
 	}
 
 	if (fall == true) {
-		if (height >= ofGetHeight() * 0.75){
+		if (triangle_locate_height >= ofGetHeight() * 0.65){
 			fall = false;
-			height =  ofGetHeight() * 0.15;
+			triangle_locate_height =  ofGetHeight() * 0.15;
 			radius = 80;
 			for (int i = 0; i < 3; i++){
 				trianglecolorNum[i] = rectcolorNum[i];
 			}
 		}
-		height += 15;
+		triangle_locate_height += 15;
 		radius -= 1;
 	}
 
@@ -60,11 +60,9 @@ void Rectfall::update(){
 //--------------------------------------------------------------
 void Rectfall::draw(){
 
-	float colorparam[3][3] = { {255, 255, 10}, {20, 246, 255}, {250, 0, 191} };
-
 	for (int i = 0; i < 3; i++) {
 		ofPushMatrix();
-		ofTranslate(length + length * i + 1, ofGetHeight() * 0.15);
+		ofTranslate(triangle_interval + triangle_interval * i + 1, ofGetHeight() * 0.15);
 		ofRotateZ(45);
 		ofSetCircleResolution(4);
 		ofSetColor(colors[rectcolorNum[i]]);
@@ -74,14 +72,14 @@ void Rectfall::draw(){
 		if (fall == true) {
 			ofSetCircleResolution(4);
 			ofSetColor(colors[rectcolorNum[i]]);
-			ofCircle(length + length * i + 1, height, radius);
+			ofCircle(triangle_interval + triangle_interval * i + 1, triangle_locate_height, radius);
 		}
 	}
 
 
 	for(int i = 0; i < 3; i++) {
 		ofPushMatrix();
-		ofTranslate(length + length * i + 1, ofGetHeight() * 0.75);
+		ofTranslate(triangle_interval + triangle_interval * i + 1, ofGetHeight() * 0.65);
 		ofRotateZ(-(triangleDegree[i]));
 		ofSetCircleResolution(3);
 		ofSetColor(colors[trianglecolorNum[i]]);
